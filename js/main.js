b1b0a44e1fc3e51e7ae5b46f7541a76f9efe9502
Poulignier Alberic <21706685t>
2019-03-13 14:43:31 +0100

    Intégration de jQuery avec l'aide de npm

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let list = new TaskList();
+let tache1 = new Task("Manger", "04/11");
+let tache2 = new Task("Dromir", "04/12");
+let tache3 = new Task("Boire", "04/10");
 let tasks = new CompoTaskList({
     element: document.getElementById('tasks'),
-    taskList: list
+    taskList: tache1, tache2, tache3
 });
