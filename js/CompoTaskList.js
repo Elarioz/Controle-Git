9ff0dc0833479662ada50bd35a4951817bd1122b
Poulignier Alberic <21706685t>
2019-03-13 14:02:01 +0100

    Déclarations JSDoc du fichier CompoTaskList.js

--- js/CompoTaskList.js
+++ js/CompoTaskList.js
@@ -1,16 +1,33 @@
 class CompoTaskList {
+    /**
+     * @summary 
+     * @param {*} param 
+     */
     constructor(param) {
         this.param = param;
     }
 
+
+    /**
+     * @summary Retourne la liste des tâches mise dans le constructeur
+     * 
+     */
     getTaskList() {
         return this.param.taskList;
     }
 
+    /**
+     * @summary Retourne l'élément du DOM passé en paramètre au constructeur
+     */
     getElement() {
         return this.param.element;
     }
 
+    /**
+     * @summary Déclare la variable children
+     * Affiche la liste des tâches dans la console et crée une div pour chaque paramètre
+     * Réitère pour chaque nouvelle tâche
+     */
     render() {
         let children = [];
         if (this.param.taskList) {
